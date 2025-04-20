5b6e1c22d385be921a33d77bf41cac2d1967fed1
Abhijay Tank <abhijaytank@outlook.com>
2025-04-20 15:47:45 +0530

    Initial commit - Express CRUD project

--- server.js
+++ server.js
@@ -1,27 +1,52 @@
-//const http = require('http')
-//const PORT = 3000
+const express = require("express");
+const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const {creds} = require('./middleware/creds')
+const errorHandler = require("./middleware/errHandler");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const rootRouter = require("./routes/root");
+const empRouter = require("./routes/api/employees");
+const regRouter = require("./routes/api/registerRoute");
+const authRouter = require("./routes/api/auth");
+const refreshTokenRouter = require("./routes/api/refresh");
+const logoutRouter = require("./routes/api/logout");
+const cookieParser = require("cookie-parser");
 
-//const server = http.createServer((req,res)=>{
- //   res.writeHead(200,{'content-type':'text/plain'
-   //     if(req.url==='/' && req.method==='GET')
-     //       res.end('Hello World')
-       // else 
-      //  {
-    //        res.writeHead(404,{'content-type':'text/plain'})
-  //      }
-//    })
-//})
+const app = express();
+const port = process.env.PORT || 3000;
 
-//server.listen(PORT,
-  //  ()=>{console.log('server is running on the localhost'+PORT)}
-//)
+//(built in middleware)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-const express = require('express')
-const app = express()
-const port = 3000
+//(custom middleware)
+app.use(logger);
+app.use(creds)
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join('./views/index.html'),{root:_dirname})
-})
+//3rd party middleware
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
-app.listen(port,()=>console.log(`server running on http://localhost:${PORT}`))
+//route
+app.use("/", rootRouter);
+app.use("/api/employees", empRouter);
+app.use("/api/register", regRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/refresh", refreshTokenRouter);
+app.use("/api/logout", logoutRouter);
+
+app.use(errorHandler);
+
+app.all(/\/*/, (req, res) => {
+  res.status(404);
+  if (req.accepts("html"))
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  else if (req.accepts("json")) res.send({ error: "404 Not Found" });
+  else res.type("txt").send("404 not found");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
